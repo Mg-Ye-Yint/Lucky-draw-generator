e6ecb0bdd389dd295f56288aaf49eda0282ddb28
Mg-Ye-Yint <tai31238@gmail.com>
2024-02-24 23:23:25 +0700

    Initial commit

--- app/root.tsx
+++ app/root.tsx
@@ -18,7 +18,17 @@ export default function App() {
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossorigin
+        ></link>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Acme&family=Wendy+One&display=swap"
+          rel="stylesheet"
+        ></link>
         <Meta />
         <Links />
       </head>
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -13,25 +13,17 @@ export const meta: MetaFunction = () => {
 export default function Index() {
   const [toggle, setToggle] = useState(false);
 
-  const [product, setProduct] = useState([
-    {
-      name: "Apple Watch",
-      quantity: 1,
-      imageUrl: "/Photo-1 (1).png",
-    },
-    {
-      name: "Vaccum Cleaner",
-      quantity: 2,
-      imageUrl: "/Photo-2 (1).png",
-    },
-    {
-      name: "Baht 1,100",
-      quantity: Infinity,
-      imageUrl: "/Photo-3 (1).png",
-    },
-  ]);
+  const [tick, setTick] = useState(false);
+
+  const [products, setProducts] = useState([]);
 
   const [won, setWon] = useState(null);
+
+  const [newItem, setNewItem] = useState({
+    name: "",
+    quantity: 0,
+    imageUrl: "",
+  });
 
   const onClick = () => {
     setToggle(!toggle);
@@ -39,56 +31,186 @@ export default function Index() {
     let randomProduct: number;
 
     do {
-      randomProduct = Math.floor(Math.random() * product.length);
-    } while (
-      product[randomProduct].quantity === 0
-      //(photoIndex === 0 && displayCount[photoIndex] >= 1) || (photoIndex === 1 && displayCount[photoIndex] >= 2)
-    );
+      randomProduct = Math.floor(Math.random() * products.length);
+    } while (products[randomProduct].quantity === 0);
 
-    const updatedProduct = [...product];
-    updatedProduct[randomProduct].quantity -= 1;
-    setProduct(updatedProduct);
+    const updatedProducts = [...products];
+    updatedProducts[randomProduct].quantity -= 1;
+    setProducts(updatedProducts);
 
-    setWon(product[randomProduct]);
+    setWon(products[randomProduct]);
   };
 
-  //console.log(won);
+  const action = () => {
+    setProducts((previousItems) => [...previousItems, newItem]);
+    setNewItem({ name: "", quantity: 0, imageUrl: "" });
+  };
+
+  const removeItem = (index) => {
+    const updatedProducts = [...products];
+    updatedProducts.splice(index, 1);
+    setProducts(updatedProducts);
+  };
+
+  const toggleInfinity = () => {
+    setNewItem((prevItem) => ({
+      ...prevItem,
+      quantity: prevItem.quantity === Infinity ? 1 : Infinity,
+    }));
+  };
+
+  const cleartext = () => {
+    setNewItem({ ...newItem, name: "" });
+  };
+
+  const tips = () => {
+    setTick(!tick);
+  };
 
   return (
     <div id="container">
-      <p>Which reward will you get?</p>
-
-      {toggle ? "" : <img id="chart" src="/chart (1).png" alt="Wheel" />}
-
-      <p id="appleWatch">
-        Apple Watch ={" "}
-        <span id="appleWatchCount">
-          {product[0]?.quantity === 0
-            ? "Out of stock"
-            : product[0]?.quantity + " item left"}{" "}
-        </span>{" "}
+      <p className="lover">
+        <b>Customize your own lucky draw</b>
       </p>
-      <p id="vacuumCleaner">
-        Vaccum Cleaner ={" "}
-        <span id="vacuumCleanerCount">
-          {product[1]?.quantity === 0
-            ? "Out of stock"
-            : product[1]?.quantity + " item left"}
-        </span>{" "}
-      </p>
-      <p>1,100 Baht = unlimited item left</p>
+
+      {toggle ? (
+        ""
+      ) : (
+        <div
+          onClick={tips}
+          onKeyDown={(e) => {
+            if (e.key === "Enter" || e.key === " ") {
+              tips();
+            }
+          }}
+          role="button"
+          tabIndex={0}
+        >
+          <img className="mainimg" src="/luckydraw.png" alt="Wheel" />
+        </div>
+      )}
+
+      {tick ? (
+        <div className="tipsContainer">
+          <h2 className="tips">
+            This is a lucky draw generator that you can used for various
+            purposes. <br />
+            You can add your items and their quanitties to your list and you can
+            remove it back too.
+            <br /> It will generate you the item you win randomly. And
+            displaying the <br />
+            quantities of items left.For items withunlimited quanitties, please
+            click the button &lsquo;Unlimited&apos;.
+          </h2>
+        </div>
+      ) : (
+        ""
+      )}
+
+      {products.map((product, index) => (
+        <p key={index}>
+          {product.name} ={" "}
+          <span className="mydiv">
+            {product.quantity === 0
+              ? "Out of stock"
+              : product.quantity === Infinity
+              ? "Unlimited Items left"
+              : product.quantity > 1
+              ? product.quantity + " items left"
+              : product.quantity + " item left"}{" "}
+            <button className="deletebutton" onClick={() => removeItem(index)}>
+              {" "}
+              X{" "}
+            </button>
+          </span>
+        </p>
+      ))}
+      <div className="Adder">
+        <label>
+          New Item:
+          <input
+            type="text"
+            placeholder="name..."
+            value={newItem.name}
+            onChange={(e) => setNewItem({ ...newItem, name: e.target.value })}
+          />
+          <div>
+            <button onClick={cleartext} className="infinitybutton">
+              Clear
+            </button>
+          </div>
+        </label>
+
+        {newItem.quantity === Infinity ? (
+          <div>
+            <div className="thistext">
+              Quantity: <br />
+              {"           "}
+              <br />
+              <div className="thistext2">Unlimited</div>
+            </div>
+            <button onClick={toggleInfinity} className="infinitybutton">
+              Limited
+            </button>
+          </div>
+        ) : (
+          <label className="quantity-input-container">
+            Quantity:
+            <input
+              type="number"
+              min="1"
+              value={newItem.quantity}
+              onChange={(e) =>
+                setNewItem({ ...newItem, quantity: Number(e.target.value) })
+              }
+            />
+            <div>
+              <button onClick={toggleInfinity} className="infinitybutton">
+                Unlimited
+              </button>
+            </div>
+          </label>
+        )}
+        {!newItem.name || !newItem.name.trim() || newItem.quantity === 0 ? (
+          <button className="addbutton"> Add</button>
+        ) : (
+          <button className="addbutton" onClick={action}>
+            {" "}
+            Add
+          </button>
+        )}
+      </div>
       <div className="itemimage">
         {toggle ? (
           won === null ? null : (
-            <img src={won.imageUrl} alt=""></img>
+            <div>
+              <img className="secimg" src="/congratulations.png" alt="" />
+              <div className="resulttext"> You got {won.name} </div>
+            </div>
           )
         ) : (
           ""
         )}
       </div>
-      <button onClick={onClick} id="button">
+      {/* <button className="spinbutton" onClick={onClick} id="button">
         {toggle ? "close" : "spin"}
-      </button>
+      </button> */}
+      <div
+        onClick={onClick}
+        onKeyDown={(e) => {
+          if (e.key === "Enter" || e.key === " ") {
+            onClick();
+          }
+        }}
+        role="button"
+        tabIndex={0}
+      >
+        {toggle ? (
+          <img className="repeat" src="/repeat.png" alt=""></img>
+        ) : (
+          <img className="buttonimg" src="/button.png" alt="" />
+        )}
+      </div>
     </div>
   );
 }
